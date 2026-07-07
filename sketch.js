59b4ad008a87ffab3d8d7e422ecde6bd75bf282e
Mahessh01 <Mahesshofficial@gmail.com>
2026-07-07 23:04:22 +0545

    javascript file

--- sketch.js
+++ sketch.js
@@ -2,6 +2,8 @@ let capture;
 let poseNet;
 let poses = [];
 
+let printed = false;
+
 function setup() {
 
     let canvas = createCanvas(760,570);
@@ -27,7 +29,10 @@ function draw() {
 
     background(0);
 
-    image(capture, 0, 0, width, height);
+    image(capture,0,0,width,height);
+
+
+    getBodyParts();
 
     drawSkeleton();
     drawKeypoints();
@@ -84,4 +89,50 @@ function drawKeypoints(){
 
     }
 
+}
+function getBodyParts() {
+
+    if (poses.length === 0) return;
+
+    let pose = poses[0].pose;
+
+    let leftShoulder = pose.leftShoulder;
+    let rightShoulder = pose.rightShoulder;
+    let leftElbow = pose.leftElbow;
+    let rightElbow = pose.rightElbow;
+    let leftHip = pose.leftHip;
+    let rightHip = pose.rightHip;
+    let leftWrist = pose.leftWrist;
+    let rightWrist = pose.rightWrist;
+
+    if (!printed) {
+
+        console.log(leftShoulder);
+        console.log(rightShoulder);
+        console.log(leftElbow);
+        console.log(rightElbow);
+        console.log(leftHip);
+        console.log(rightHip);
+        console.log(leftWrist);
+        printed = true;
+         let vector1 = {
+        x: leftShoulder.x - leftElbow.x,
+        y: leftShoulder.y - leftElbow.y
+        }
+        let vector2 = {
+        x: leftWrist.x - leftElbow.x,
+        y: leftWrist.y - leftElbow.y
+        };
+        let length1 = Math.sqrt(
+            vector1.x * vector1.x +
+            vector1.y * vector1.y
+        );
+
+        let length2 = Math.sqrt(
+            vector2.x * vector2.x +
+            vector2.y * vector2.y
+        );
+        let dotProduct =(vector1.x * vector2.x) + (vector1.y * vector2.y);
+        console.log("Dot Product:", dotProduct);
+    }
 }
